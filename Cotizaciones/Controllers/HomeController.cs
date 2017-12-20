7ce789cba0ad84c4ab639235a9283c1438413fdd
calvarezrodriguez <33533317+calvarezrodriguez@users.noreply.github.com>
2017-12-20 12:26:23 -0300

    eliminando un archivo del repositorio

---
+++ Cotizaciones/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Diagnostics; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.Extensions.Logging; 
+using Microsoft.AspNetCore.Mvc; 
+using Lab.Models; 
+
+namespace Lab.Controllers {
+    public class HomeController:Controller {
+        private readonly ILogger _logger; 
+
+        public HomeController(ILogger < HomeController > logger) {
+            _logger = logger; 
+        }
+        
+        public IActionResult Index() {
+            _logger.LogInformation("In Index we are!");
+            return View(); 
+        }
+
+        public IActionResult About() {
+            ViewData["Message"] = "Your application description page."; 
+
+            return View(); 
+        }
+
+        public IActionResult Contact() {
+            ViewData["Message"] = "Your contact page."; 
+
+            return View(); 
+        }
+
+        public IActionResult Error() {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id??HttpContext.TraceIdentifier }); 
+        }
+    }
+}
