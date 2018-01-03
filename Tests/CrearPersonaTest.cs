8f291fd8a9bf575c7fe10ecfe431e010b184a86f
JulioVit <33534518+JulioVit@users.noreply.github.com>
2018-01-02 21:46:52 -0300

    Tests preparados
    Agregado el contexto de los Tests a realizar, ademas de la prueba de Crear Persona a la que le falta por ahora la conexión a la base de datos

---
+++ Tests/CrearPersonaTest.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cotizaciones.Models;
+using Cotizaciones.Controllers;
+using Cotizaciones.Data;
+using Xunit;
+using static Cotizaciones.Models.Persona;
+
+namespace PersonaControllerTests
+{
+    public class CrearPersonaTest
+    {
+        private readonly CotizacionesContext _context;
+        private PersonaController personaController;
+        public CrearPersonaTest()
+        {
+            //_context = ???
+            this.personaController = new PersonaController(_context);
+        }
+        
+        [Fact]
+        public async Task CreateAValidPersona()
+        {
+            int testId = 73;
+            string testNombre = "Felipe";
+            string testRut = "182345679";
+            string testPaterno = "Rojas";
+            string testMaterno = "Villaroel";
+            Persona persona = new Persona(testId,testNombre,testRut,testPaterno,testMaterno);
+            var result = await personaController.Create(persona);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnSession = Assert.IsType<Persona>(okResult.Value);
+            Assert.NotNull(returnSession);
+            Assert.Equal(73, persona.Id);
+            Assert.Equal("Felipe", persona.Nombre);
+            Assert.Equal("182345679", persona.Rut);
+            Assert.Equal("Rojas", persona.Paterno);
+            Assert.Equal("Villaroel", persona.Materno);
+        }
+    }
+}
